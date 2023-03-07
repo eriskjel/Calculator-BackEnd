18ecc42fe3be4b2fcd9b0b1fad545b8609be9b68
eriskjel <eriskjel@stud.ntnu.no>
2023-03-07 16:38:04 +0100

    created user class

--- src/main/java/ntnu/models/User.java
+++ src/main/java/ntnu/models/User.java
@@ -1,2 +1,55 @@
-package ntnu.models;public class User {
+package ntnu.models;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username=" + username + ", password=" + password + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
