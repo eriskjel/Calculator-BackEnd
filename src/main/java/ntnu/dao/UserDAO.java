65892a34635ceefccc04fed08a2dc7884411d0cf
eriskjel <eriskjel@stud.ntnu.no>
2023-03-07 16:38:20 +0100

    created userDAO class

--- src/main/java/ntnu/dao/UserDAO.java
+++ src/main/java/ntnu/dao/UserDAO.java
@@ -1,2 +1,18 @@
-package ntnu.dao;public class UserDAO {
+package ntnu.dao;
+
+import ntnu.models.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class UserDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void saveUser(User user) {
+        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        jdbcTemplate.update(sql, user.getUsername(), user.getPassword());
+    }
 }
