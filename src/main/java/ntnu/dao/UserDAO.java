e0b7fb07e18f8efa3d0de0eb93ae561262565f69
eriskjel <eriskjel@stud.ntnu.no>
2023-03-07 18:00:58 +0100

    created UserDAO and UserDAOImpl files

--- src/main/java/ntnu/dao/UserDAO.java
+++ src/main/java/ntnu/dao/UserDAO.java
@@ -1,18 +1,12 @@
 package ntnu.dao;
 
 import ntnu.models.User;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-public class UserDao {
+import java.util.List;
 
-    private final JdbcTemplate jdbcTemplate;
+public interface UserDAO {
+    int saveUser(User user);
+    User findUserById(int id);
 
-    public UserDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public void saveUser(User user) {
-        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-        jdbcTemplate.update(sql, user.getUsername(), user.getPassword());
-    }
+    List<User> getAllUsers();
 }
---
+++ src/main/java/ntnu/dao/UserDAOImpl.java
@@ -0,0 +1,65 @@
+package ntnu.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Objects;
+
+import ntnu.models.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserDAOImpl implements UserDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int saveUser(User user) {
+        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            return ps;
+        }, keyHolder);
+        user.setId(Objects.requireNonNull(keyHolder.getKey()).intValue()); // update the id on the user object
+        return user.getId(); // return the generated id
+    }
+
+    @Override
+    public User findUserById(int id) {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{id}, new UserRowMapper());
+    }
+
+    public void deleteAllUsers() {
+        String sql = "DELETE FROM users";
+        jdbcTemplate.update(sql);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        String sql = "SELECT * FROM users";
+        return jdbcTemplate.query(sql, new UserRowMapper());
+    }
+    private static final class UserRowMapper implements RowMapper<User> {
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            int id = rs.getInt("id");
+            String username = rs.getString("username");
+            String password = rs.getString("password");
+            return new User(id, username, password);
+        }
+    }
+}
